be709f44c8e3b1b195a67f0f4b739f9a4ef57e78
ITZCHERRY111 <86556904+ITZCHERRY111@users.noreply.github.com>
2025-04-30 16:28:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,5 +39,5 @@ if options:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_wdth=TRUE)
+#st.text(smoothiefroot_response.json())
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_wdth=True)
